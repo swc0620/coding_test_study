ecfc724bab342a23de4637752897dc624cccd2e4
jihyunhong <98jihyun@naver.com>
2021-06-29 12:54:35 +0900

    백준 1927 홍지현

---
+++ baekjoon/1927_최소힙.cpp
@@ -0,0 +1,35 @@
+// 1927 최소힙
+
+#include <iostream>
+#include <queue>
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+    // greater<int> : 오름차순
+    std::priority_queue<int, std::vector<int>, std::greater<int> > p_queue;
+    
+    int N;
+    std::cin >> N;
+
+    for (int i=0; i<N; i++){
+        int temp;
+        std::cin >> temp;
+
+        if (temp == 0){
+            if (p_queue.empty()){
+                std::cout << "0" << "\n";
+            }
+            else{
+                std::cout << p_queue.top() << "\n";
+                p_queue.pop();
+            }
+        }
+        else{
+            p_queue.push(temp);
+        }
+    }
+    return 0;
+}
