180df6ef816f5e9331af88e43c145d61a5f0929a
jihyunhong <98jihyun@naver.com>
2021-06-29 12:51:41 +0900

    백준 1158 홍지현

---
+++ baekjoon/1158_요세푸스.cpp
@@ -0,0 +1,29 @@
+// baekjoon 1158 요세푸스 
+
+#include <iostream>
+#include <queue>
+
+int main(){
+    int N, K;
+    std::cin >> N >> K;
+    std::queue<int> q;
+
+    // 7 3
+    for (int i=1; i<N+1; i++){
+        q.push(i);
+    }
+
+    std::cout << "<";
+    while (q.size()-1 > 0){
+        for (int i=0; i<K-1; i++){ // 위에서 2개의 수를 제거하여 마지막에 다시 삽입
+            q.push(q.front());
+            q.pop();
+        }
+        std::cout << q.front() << ", ";
+        q.pop();
+    }
+    std::cout << q.front() <<  ">";
+    return 0;
+}
+
+// 출력: <3, 6, 2, 7, 5, 1, 4>
