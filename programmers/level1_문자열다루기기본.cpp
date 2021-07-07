6d4dc3c3fde327d360011fd7f76a2294f085b987
지현 <jihyun@jihyeon-ui-MacBookAir.local>
2021-07-08 01:55:00 +0900

    프로그래머스(level 1) 문자열다루기기본 홍지현

---
+++ programmers/level1_문자열다루기기본.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    if (s.size() != 4 && s.size() != 6){
+        return false;
+    }
+    
+    for (int i=0; i<s.size(); i++){
+        if (s[i] > '9'){
+            answer = false;
+            break;
+        }
+    }
+    return answer;
+}
