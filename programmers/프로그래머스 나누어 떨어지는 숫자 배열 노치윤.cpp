9317f3d010c33eb2598b9736f645d4381d685fa1
ChiyunNoh <80042772+ChiyunNoh@users.noreply.github.com>
2021-07-07 14:47:02 +0900

    프로그래머스 나누어 떨어지는 숫자 배열 노치윤

---
+++ programmers/프로그래머스 나누어 떨어지는 숫자 배열 노치윤.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+
+    for (int a : arr)
+    {
+        if (a % divisor == 0)
+            answer.push_back(a);
+    }
+    if (answer.size() == 0)
+        answer.push_back(-1);
+    sort(answer.begin(), answer.end());
+
+    return answer;
+}
