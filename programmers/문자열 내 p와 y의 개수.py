4164cd70885479e66e49ba05c2658c67af10289c
aeddung <48937254+aeddung@users.noreply.github.com>
2021-07-08 21:34:43 +0900

    프로그래머스 문자열 내 p와 y의 개수 황준혁

---
+++ programmers/문자열 내 p와 y의 개수.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = True
+    
+    s = s.lower()
+    p_count = s.count('p')
+    y_count = s.count('y')
+    if p_count == y_count:
+        return True
+    else:
+        return False
