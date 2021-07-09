5daf9c689caf4259f8af2339b88bad4112251deb
suehyunpark <tngus1499@gmail.com>
2021-07-09 14:19:22 +0900

    프로그래머스 수박수박수박수박수박수?  박수현

---
+++ programmers_level1/수박수박수박수박수박수?.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for (int i=1; i<=n; ++i) {
+        answer += (i%2 == 1) ? "수" : "박";
+    }
+    return answer;
+}
