845293f240fc51bd9046d10d1ef8865b44fe856a
swc0620 <swc0620@github.com>
2021-07-18 22:16:53 +0900

    프로그래머스 더맵게 신우철

---
+++ pro_56_더_맵게.py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville) * 2)
+        answer += 1
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
+    return answer
+
+# scoville = list(map(int, input().split(',')))
+# K = int(input())
+# print(solution(scoville, K))
