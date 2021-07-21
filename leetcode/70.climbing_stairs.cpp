dd477100592fd02675efa006b894febf2553ea7c
지현 <jihyun@jihyeon-ui-MacBookAir.local>
2021-07-21 21:30:39 +0900

    leetcode climbing_staris 홍지현

---
+++ leetcode/70.climbing_stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+        
+        int ans[n+1];
+        ans[1]=1;
+        ans[2]=2;
+        
+        for(int i=3;i<=n;i++) {
+            ans[i]=ans[i-1]+ans[i-2];
+        }
+        
+        return ans[n];
+    }
+};
