22873fc38fad91facf9cd6f511e3b066a3288343
aeddung <48937254+aeddung@users.noreply.github.com>
2021-07-21 23:30:11 +0900

    프로그래머스 124 나라의 숫자 황준혁

---
+++ programmers/124 나라의 숫자.py
@@ -0,0 +1,11 @@
+# 참고 코드
+def solution(n):
+    num = ['1', '2', '4']
+    answer = ""
+
+    while n > 0:
+        n -= 1
+        answer = num[n % 3] + answer
+        n //= 3
+
+    return answer
