d931032b7edf8de54651bba7f563c83c83408922
ChiyunNoh <80042772+ChiyunNoh@users.noreply.github.com>
2021-07-24 15:20:52 +0900

    LeetCode Best Time to Buy and Sell Stock 노치윤

---
+++ Leet Code/LeetCode Best Time to Buy and Sell Stockl 노치윤.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        int min=prices[0];
+        for(int i=0;i<prices.length;i++){
+            if(min>prices[i]) min=prices[i];
+            if(max<prices[i]-min) max=prices[i]-min;
+        }
+        return max;
+        
+    }
+}
