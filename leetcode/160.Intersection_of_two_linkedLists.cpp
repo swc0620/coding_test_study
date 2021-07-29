d93fbccca122b1a682ef3b4da074a79567f36a60
지현 <jihyun@jihyeon-ui-MacBookAir.local>
2021-07-29 23:03:25 +0900

    leetcode 160.intersection_of_two_linkedLists 홍지현 수정

--- leetcode/160.Intersection_of_two_linkedLists.cpp
+++ leetcode/160.Intersection_of_two_linkedLists.cpp
@@ -1,29 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
+    ListNode *compute(int d, ListNode *a, ListNode *b){
+        ListNode *h1 = a, *h2 = b;
+        for(int i=0;i<d;i++){
+            if(h1 == NULL) return NULL;
+            h1 = h1->next;
+        }
+        while(h1 and h2){ 
+            if(h1 == h2)
+                return h1;
+            h1 = h1->next;
+            h2 = h2->next;
+        }
+        return NULL;
+    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if (headA == NULL || headB == NULL)
-            return NULL;
-        if (headA == headB)
-            return headA;
-		ListNode *curA = headA;
-		ListNode *curB = headB;
-		int lenA = std::numeric_limits<int>::max() / 2, lenB = lenA;
-		for (int i = 0; i != lenA + lenB - 1; ++i)
-			if (curA->next)
-				curA = curA->next;
-			else {
-				curA = headB;
-				lenA = i + 1;
-			}
-			if (curB->next)
-				curB = curB->next;
-			else {
-				curB = headA;
-				lenB = i + 1;
-			}
-			if (curA == curB)
-				return curA;
-		}
-		return NULL;
+        ListNode *curr1 = headA, *curr2 = headB;
+        int c1=0, c2 = 0;
+        while(curr1){
+            c1++;
+            curr1=curr1->next;
+        }
+        while(curr2){
+            c2++;
+            curr2=curr2->next;
+        }
+        
+        if(c1>c2){
+            int d = c1-c2;
+            return compute(d,headA,headB);
+        }
+        else{
+            int d = c2-c1;
+            return compute(d,headB,headA);
+        }
+        return NULL;
     }
 };
