710ae146925dc9d7d3e1162b6dc7d8e1cae8f2ca
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2021-07-29 23:46:47 +0900

    programmers 카펫.java

---
+++ programmers/카펫.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {0,0};
+        int yelloww=yellow;
+        int yellowh=1;
+        answer[0]=yelloww+2;
+        answer[1]=yellowh+2;
+        for(int i=2;i<yellow;i++){
+            if(2*(yelloww+2)+2*yellowh==brown){
+                answer[0]=yelloww+2;
+                answer[1]=yellowh+2;
+                break;
+            }
+            if(yellow%i==0){
+                yelloww=yellow/i;
+                yellowh=i;
+            }
+        }
+        if(answer[0]<answer[1]){
+            int tmp = answer[0];
+            answer[0]=answer[1];
+            answer[1]=tmp;
+        }
+        return answer;
+    }
+}
