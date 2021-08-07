77a648c1aef25eb54e1100d72beee78796e16bd3
지현 <jihyun@jihyeon-ui-MacBookAir.local>
2021-08-07 23:07:03 +0900

    leetcode 617.merge_two_binary_trees 홍지현

--- leetcode/617.merge_two_binary_trees.cpp
+++ leetcode/617.merge_two_binary_trees.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        TreeNode* newRoot = t1;
+        TreeNode* newRoot = t1; 
 
         if(t1 == NULL) return t2;
         if(t2 == NULL) return t1;
