70385a1672c521adacc1a81d133bcbfc0481c0e3
swc0620 <swc0620@github.com>
2021-08-11 23:56:36 +0900

    Leetcode 2AddTwoNumbers 신우철

---
+++ leetcode_2_Add_Two_Numbers.py
@@ -0,0 +1,13 @@
+def solution(skill, skill_trees):
+    answer = 0
+    for tree in skill_trees:
+        temp = list(skill)
+        for i in tree:
+            if i in temp and i == temp[0]:
+                temp.pop(0)
+                continue
+            elif i in temp and i != temp[0]:
+                break
+        else:
+            answer += 1
+    return answer
