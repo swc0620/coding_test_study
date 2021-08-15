f277efea5ad95632349a3e93b1a6bd7d11610f31
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2021-08-15 23:49:13 +0900

    leetcode 3sum 권윤영

--- leetcode/3sum.java
+++ leetcode/3sum.java
@@ -1,24 +1,21 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> set = new HashSet<>();
-        Set<Integer> threeSum = new HashSet<>();
-        
-        Arrays.sort(nums);
-        
-        if(nums.length < 3) {
-            return new ArrayList<>();
-        }
-        
-        for(int i=0; i<nums.length; i++) {
-            int result = -nums[i];
-            threeSum.clear();
-            for(int j=i+1; j<nums.length; j++) {
-                if(threeSum.contains(result - nums[j])) {
-                    set.add(Arrays.asList(nums[i], nums[j], (result - nums[j])));
-                }
-                threeSum.add(nums[j]);
+    public List<List<Integer>> threeSum(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> res = new LinkedList<>(); 
+        for (int i = 0; i < num.length-2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
+               }
             }
         }
-        return new ArrayList<>(set);
-    } 
+        return res;
+    }
 }
