d4740ad859fac61b7de7643c2eabbeaebed869dc
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2021-08-18 23:55:20 +0900

    leetcode generate paranthesis 권윤영

---
+++ leetcode/22.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        helper(res,n,0,"");
+        return res;
+    }
+    
+    private void helper(List<String> res, int left, int right, String s) {
+        if (left == 0 && right == 0) {
+            res.add(s);
+            return;
+        }
+        if (left > 0) helper(res,left - 1, right + 1, s + "(");
+        if (right > 0) helper(res,left, right - 1, s + ")");
+    }
+}
