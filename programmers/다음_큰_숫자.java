b25879c88bcc5ba5bddccbaaec9831c820340773
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2021-08-21 23:54:58 +0900

    programmers 다음 큰 숫자 권윤영

---
+++ programmers/다음_큰_숫자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int count = Integer.bitCount(n);
+        while(true){
+            n++;
+            if(count == Integer.bitCount(n)) break;
+        }
+        return n;
+    }
+}
