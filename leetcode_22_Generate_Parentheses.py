7cdf7670d4f9f5f3492b3b59d57feff5f21ac17a
swc0620 <swc0620@github.com>
2021-08-23 17:20:52 +0900

    Leetcode 22GenerateParentheses 신우철

--- leetcode_22_Generate_Parentheses.py
+++ leetcode_22_Generate_Parentheses.py
@@ -1,7 +1,7 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         def generate(p, left, right, parens=[]):
-            if left:
+            if left > 0:
                 generate(p + '(', left-1, right)
             if right > left:
                 generate(p + ')', left, right-1)
