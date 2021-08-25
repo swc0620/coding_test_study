2c8c92fd79b448a707506babdc070fad69beb67c
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2021-08-25 22:39:46 +0900

    programmers 최댓값과 최솟값 권윤영

---
+++ programmers/최댓값과_최솟값.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+public class Solution {
+  public static void main(String[] args) {
+		String s = "1 2 3 4";
+		String s2 = "-1 -2 -3 -4";
+		String s3 = "-1 -1";
+		
+		System.out.println(solution(s));
+	}
+	public static String solution(String s) {
+		String[] arr = s.split(" ");
+		ArrayList<Integer> nums = new ArrayList<>();
+		for(int i = 0; i < arr.length; i++) {
+			nums.add(Integer.parseInt(arr[i]));
+		}
+		
+		Collections.sort(nums);
+		
+		return String.valueOf(nums.get(0)) + " " + String.valueOf(nums.get(nums.size() - 1));
+	}
+}
