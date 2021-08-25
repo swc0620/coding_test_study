0498b6b43060532c617981a3a72e788309213f25
swc0620 <swc0620@github.com>
2021-08-25 15:31:00 +0900

    프로그래머스 땅따먹기 신우철

---
+++ pro_101_땅따먹기.py
@@ -0,0 +1,12 @@
+def solution(land):
+    for i in range(1, len(land)):
+        for j in range(len(land[i])):
+            temp = land[i-1][:]
+            temp.pop(j)
+            land[i][j] += max(temp)
+    return max(land[-1])
+
+lands = []
+for _ in range(3):
+    lands.append(list(map(int, input().split())))
+print(solution(lands))
