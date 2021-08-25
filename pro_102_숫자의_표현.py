8734ca0f4ffb6fca5c54defbb9d70b24194ac842
swc0620 <swc0620@github.com>
2021-08-25 16:13:33 +0900

    프로그래머스 숫자의표현 신우철

--- pro_102_숫자의_표현.py
+++ pro_102_숫자의_표현.py
@@ -4,15 +4,12 @@ def solution(n):
     while num <= n:
         i += 1
         num += i
-    print(i)
     for j in range(1, i):
         if j % 2 == 1:
-            print(j, n/j, n//j)
             if n / j == n // j:
                 answer += 1
         else:
             q, r = divmod(n, j)
-            print(j, r, j/2)
             if r == j / 2:
                 answer += 1
     return answer
