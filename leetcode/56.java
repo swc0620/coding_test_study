a46b21763fa30e3c640de4e9a5aa0f421de615d2
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2021-08-28 23:54:58 +0900

    leetcode merge intervals 권윤영

---
+++ leetcode/56.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        LinkedList<Interval> merged = new LinkedList<>();
+        if (intervals == null || intervals.size() == 0) {
+            return merged;
+        }
+        Collections.sort(intervals, (a, b) -> a.start - b.start);
+        for (Interval interval : intervals) {
+            if (merged.isEmpty() || merged.getLast().end < interval.start) {
+                merged.add(interval);
+            } else {
+                merged.getLast().end = Math.max(merged.getLast().end, interval.end);
+            }
+        }
+        return merged;
+    }
+}
