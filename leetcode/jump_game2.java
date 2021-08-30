8667bfbadd523a42a1d24c847bae80a5c6a24fef
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2021-08-30 20:24:28 +0900

    쓸모없는 주석  제거

--- leetcode/jump_game2.java
+++ leetcode/jump_game2.java
@@ -3,14 +3,13 @@ class Solution {
         if(nums.length==0) return 0;
         
         int[] dp=new int[nums.length];
-        Arrays.fill(dp, Integer.MAX_VALUE); // Math.min을 사용하기 위함
+        Arrays.fill(dp, Integer.MAX_VALUE); 
         dp[0]=0;
-        for(int i=0;i<nums.length;i++){ // i 위치에서 다 체크
-            for(int j=1;j<=nums[i] && i+j<nums.length;j++){ // 1부터 nums[i]까지 뛰어본다, 단 배열 범위 벗어나지 않는 범위내에서
-                dp[i+j]=Math.min(dp[i+j], dp[i]+1); // 기존 점프 횟수와 i에서 뛴 것 중 작은 값 선택
+        for(int i=0;i<nums.length;i++){ 
+            for(int j=1;j<=nums[i] && i+j<nums.length;j++){ 
+                dp[i+j]=Math.min(dp[i+j], dp[i]+1); 
             }
         }
-        
         return dp[nums.length-1];
     }
 }
