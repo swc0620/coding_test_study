73ba6ffd635e0d44c8e8c045dfe355285e3eec96
suehyunpark <tngus1499@gmail.com>
2021-08-30 19:02:38 +0900

    LeetCode Unique_Paths 박수현

---
+++ leetcode/Unique_Paths.java
@@ -0,0 +1,14 @@
+class Solution {
+  public int uniquePaths(int m, int n) {
+      int[][] dp = new int[m][n];
+      
+      for (int i=0; i<m; ++i) {
+          for (int j=0; j<n; ++j) {
+              // either way: ↓ → or → ↓
+              dp[i][j] = (i==0 || j==0) ? 1 : dp[i-1][j] + dp[i][j-1]; // the outer border has a single straightforward path
+          }
+      }
+      
+      return dp[m-1][n-1];
+  }
+}
