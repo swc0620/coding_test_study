7870261553cdbad9992eb0145d0a300883be4865
swc0620 <pinetreewave@naver.com>
2021-10-02 22:15:27 +0900

    백준1026 보물

---
+++ boj_1026_보물.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+
+res = 0
+for a, b in zip(A, B):
+    res += a * b
+
+print(res)
