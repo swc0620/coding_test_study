7ef567c95c8f00bb667d08bf573b0c898085ef2d
swc0620 <pinetreewave@naver.com>
2021-12-19 14:35:54 +0900

    백준2490 윷놀이

---
+++ boj_2490_윷놀이.py
@@ -0,0 +1,12 @@
+for _ in range(3):
+    yut = sum(list(map(int, input().split())))
+    if yut == 0:
+        print('D')
+    elif yut == 1:
+        print('C')
+    elif yut == 2:
+        print('B')
+    elif yut == 3:
+        print('A')
+    elif yut == 4:
+        print('E')
