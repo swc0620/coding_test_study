2071ad15efe8226b8ce1789641db6138a2cef64a
swc0620 <59383615+swc0620@users.noreply.github.com>
2021-12-26 02:24:05 +0900

    백준18258 큐2

---
+++ boj_18258_큐_2.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+queue = deque()
+for _ in range(N):
+    cmd = input().rstrip().split()
+    if cmd[0] == 'push':
+        queue.append(int(cmd[1]))
+        continue
+    elif cmd[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(queue))
+    elif cmd[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'front':
+        if queue:8
+            print(queue[0])
+        else:
+            print(-1)
+    elif cmd[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
